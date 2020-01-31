2015137750332bfb6144d2d7bbb34108b14c679b
davidcraig <davidcraig1986@gmail.com>
2020-01-31 15:09:43 +0000

    Exclude test files in babel ignore.

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,20 @@
 // babel.config.js
+let NodeEnv = process.env.NODE_ENV
+let BabelIgnore = [
+  "__snapshots__/**"
+]
+
+if (NodeEnv == 'test') {
+} else {
+  BabelIgnore.push("**/*.test.js")
+  BabelIgnore.push("**/*.spec.js")
+}
+
 module.exports = {
   presets: [
     '@babel/preset-env',
     '@babel/preset-react'
   ],
-  ignore: [
-    "**/*.test.js",
-    "**/*.spec.js"
-  ],
+  ignore: BabelIgnore,
   plugins: ["react-docgen"]
 };
